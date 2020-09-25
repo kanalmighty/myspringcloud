0a482d9dae87a37d0f642bf918966c8a42d1ffc9
kanalmighty <232701147@qq.com>
2020-09-25 14:45:37 +0800

    login module

--- cloudalibaba-authentication-service8006/src/main/java/com/zk/cloudalibaba/cloudalibabaauthmain8006.java
+++ cloudalibaba-authentication-service8006/src/main/java/com/zk/cloudalibaba/cloudalibabaauthmain8006.java
@@ -1,4 +1,4 @@
-package com.zk.cloudalibaba.config;
+package com.zk.cloudalibaba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cloudalibaba-authentication-service8006/src/main/java/com/zk/cloudalibaba/config/SecurityConfig.java
+++ cloudalibaba-authentication-service8006/src/main/java/com/zk/cloudalibaba/config/SecurityConfig.java
@@ -1,18 +1,59 @@
 package com.zk.cloudalibaba.config;
 
 import com.sun.javaws.security.JavaWebStartSecurity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     //重写方法
     protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic()
+        security.csrf().disable()//接触跨站攻击防御
+                .formLogin()
+                .loginPage("/login.html")//登录页面
+                .loginProcessingUrl("/login")//登录url
+                .defaultSuccessUrl("/index.html")//登录以后跳转页面
                 .and()
-                .authorizeRequests().anyRequest().authenticated();//所有的资源必须在登录之后才可以访问
+                .authorizeRequests()
+                .antMatchers("/login.html","/login").permitAll()//这两个资源对所有请求开放
+                .antMatchers("/biz1", "/biz2")
+                .hasAnyAuthority("ROLE_user","ROLE_admin")//以上两个资源必须有user和admin权限可以访问
+                .antMatchers("/syslog", "/sysuser")
+                .hasAnyAuthority("sys:log")
+                .anyRequest().authenticated();
+    }
+
+    //配置用户权限角色信息
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("12345"))
+                .roles("user")
+                    .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("12345"))
+                .roles("admin")
+                    .and()
+                .passwordEncoder(passwordEncoder());
+    }
+
+    //对用户密码进行加密
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //静态资源不做权限控制
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/*","/image/*");//静态资源不做权限控制
     }
 }
